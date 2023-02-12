f83f7f578ce173e357a5af3fa9c802743db39fa1
Finlay Smith <31561608+FinnSm1th@users.noreply.github.com>
2023-02-12 15:32:27 +0000

    Added basic functionality
    5 random quotes display onto homepage when logged in using breeze authentication.

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Http;
 
 class PageController extends Controller
 {
-    //
+    // function to get 5 quotes from rest api and return to index view
+    public function index()
+    {
+        $quotes = [];
+        for ($i = 0; $i < 5; $i++) {
+            $response = Http::get('https://api.kanye.rest');
+            $quotes[] = $response->json()['quote'];
+        }
+        
+        return view('index', [
+            'quotes' => $quotes
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', [PageController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
