35e4333e33f684bb564988ad9e8d68f1a3f0b51c
Finlay Smith <31561608+FinnSm1th@users.noreply.github.com>
2023-02-13 15:48:51 +0000

    Added logic to prevent duplicate quotes appearing.
    Added nested loops to check array for duplicate quotes before new quotes are added.
    Changed post to get so that responses may be cached.
    Removed breeze default styling.

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,13 +7,30 @@ use Illuminate\Support\Facades\Http;
 
 class PageController extends Controller
 {
-    // function to get 5 quotes from rest api and return to index view
+    // function to get 5 unique quotes from rest api and return to 'quote' view.
     public function quotes()
     {
         $quotes = [];
+        // loop adds quotes to array until there are 5 unique values.
         for ($i = 0; $i < 5; $i++) {
-            $response = Http::post('https://api.kanye.rest');
-            $quotes[] = $response->json()['quote'];
+            $response = Http::get('https://api.kanye.rest');
+            $newQuote = $response->json()['quote'];
+            $isDuplicate = false;
+            if ($i != 0) { // first quote always placed into array
+                // looping through array to check if quotes are duplicates
+                for ($j = 0; $j < count($quotes); $j++) {
+                    if ($newQuote == $quotes[$j]) {
+                        $i--; // don't progess in parent for loop
+                        $isDuplicate = true; // prevents duplicate from being added
+                        continue;
+                    }
+                }
+            }
+            if ($isDuplicate == true) {
+                continue;
+            } else {
+                $quotes[] = $newQuote;
+            }
         }
         
         return view('quotes', [
